5bc8e46e43c7486d1080883f0f0db1173c241c8d
webdevsmart <webdevsmart@hotmail.com>
2020-04-13 05:40:23 +0900

    ddd

--- system/library/msmail.php
+++ system/library/msmail.php
@@ -89,6 +89,7 @@ class MsMail extends Model {
 		print_r($seller_id.'br');
 		print_r('order_id:'.$order_id.'br');
 		//print_r('mails:'.$mails);
+		$order_info['comment'] = $this->MsLoader->MsOrderData->getOrderComment(array('order_id' => $order_id, 'seller_id' => $seller_id));
 		print_r($order_info['comment']);
 
 		$mailTransport->sendMails($mails);
