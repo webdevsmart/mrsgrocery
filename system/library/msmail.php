c0ce161fe1356dc89fa9c0a8466bfc5b390c8ab5
webdevsmart <webdevsmart@hotmail.com>
2020-04-13 15:37:26 +0900

    msmail, purchased

--- system/library/msmail.php
+++ system/library/msmail.php
@@ -20,10 +20,7 @@ class MsMail extends Model {
 	}
 
 	public function sendOrderMails($order_id) {
-		print_r('<-----------msmail info--------------------->');
 		$serviceLocator = $this->MsLoader->load('\MultiMerch\Module\MultiMerch')->getServiceLocator();
-		
-
 		$mailTransport = $serviceLocator->get('MailTransport');
 		$mails = new \MultiMerch\Mail\Message\MessageCollection();
 		$order_products = $this->_getOrderProducts($order_id);
@@ -43,53 +40,55 @@ class MsMail extends Model {
 			$order_info = $this->model_sale_order->getOrder($order_id);
 		}
 		$this->load->model('tool/upload');
+
+		$seller_array = array();
+		
 		foreach ($order_products as $product) {
 			$seller_id = $this->MsLoader->MsProduct->getSellerId($product['product_id']);
 			if ($seller_id) {
-				/** @see \MsOrderData::getOrderProducts */
-				$orderSellerProducts = $this->MsLoader->MsOrderData->getOrderProducts(array('order_id' => $order_id, 'seller_id' => $seller_id));
-				foreach ($orderSellerProducts as $oSkey => $oSp) {
-					if ($this->_modelExists('account/order')) {
-						$options = $this->model_account_order->getOrderOptions($order_id, $oSp['order_product_id']);
-					} else {
-						$options = $this->model_sale_order->getOrderOptions($order_id, $oSp['order_product_id']);
-					}
-					$option_data = array();
-					foreach ($options as $option)
-					{
-						if ($option['type'] == 'file') {
-							$upload_info = $this->model_tool_upload->getUploadByCode($option['value']);
-							if ($upload_info) {
-								$option['value']	=  $upload_info['name'];
-							}else{
-								$option['value'] = '';
-							}
+				if(!array_search($seller_id, $seller_array)) {
+					array_push($seller_array, $seller_id);
+					/** @see \MsOrderData::getOrderProducts */
+					$orderSellerProducts = $this->MsLoader->MsOrderData->getOrderProducts(array('order_id' => $order_id, 'seller_id' => $seller_id));
+					foreach ($orderSellerProducts as $oSkey => $oSp) {
+						if ($this->_modelExists('account/order')) {
+							$options = $this->model_account_order->getOrderOptions($order_id, $oSp['order_product_id']);
+						} else {
+							$options = $this->model_sale_order->getOrderOptions($order_id, $oSp['order_product_id']);
 						}
-						$option_data[] = $option;
+						$option_data = array();
+						foreach ($options as $option)
+						{
+							if ($option['type'] == 'file') {
+								$upload_info = $this->model_tool_upload->getUploadByCode($option['value']);
+								if ($upload_info) {
+									$option['value']	=  $upload_info['name'];
+								}else{
+									$option['value'] = '';
+								}
+							}
+							$option_data[] = $option;
+						}
+						$orderSellerProducts[$oSkey]['order_options'] = $option_data;
 					}
-					$orderSellerProducts[$oSkey]['order_options'] = $option_data;
+
+					$total = $this->MsLoader->MsOrderData->getOrderTotal($order_id, array('seller_id' => $seller_id));
+
+					$order_info['comment'] = $this->MsLoader->MsOrderData->getOrderComment(array('order_id' => $order_id, 'seller_id' => $seller_id));
+					$MailProductPurchased = $serviceLocator->get('MailProductPurchased', false)
+						->setTo($this->MsLoader->MsSeller->getSellerEmail($seller_id))
+						->setData(array(
+							'addressee' => $this->MsLoader->MsSeller->getSellerName($seller_id),
+							'order_products' => $orderSellerProducts,
+							'total' => $total,
+							'order_info' => $order_info,
+						));
+					$mails->add($MailProductPurchased);
 				}
-
-				$total = $this->MsLoader->MsOrderData->getOrderTotal($order_id, array('seller_id' => $seller_id));
-
-				$order_info['comment'] = $this->MsLoader->MsOrderData->getOrderComment(array('order_id' => $order_id, 'seller_id' => $seller_id));
-				$MailProductPurchased = $serviceLocator->get('MailProductPurchased', false)
-					->setTo($this->MsLoader->MsSeller->getSellerEmail($seller_id))
-					->setData(array(
-						'addressee' => $this->MsLoader->MsSeller->getSellerName($seller_id),
-						'order_products' => $orderSellerProducts,
-						'total' => $total,
-						'order_info' => $order_info,
-					));
-				$mails->add($MailProductPurchased);
 			}
 		}
-		$seller_id = $this->MsLoader->MsProduct->getSellerId(3457);
 		print_r('<-----------msmail info--------------------->');
-		print_r($order_info);
-		print_r($mails->getList());
 		$mailTransport->sendMails($mails);
-
 		die();
 	}
 }
--- system/vendor/multimerchlib/module/catalog/view/theme/default/msmail/product/purchased.tpl
+++ system/vendor/multimerchlib/module/catalog/view/theme/default/msmail/product/purchased.tpl
@@ -17,7 +17,7 @@ foreach ($this->order_products as $p) {
 }
 ?>
 <?php if ($this->getMultiMerchModule()->getConfigOC('msconf_hide_customer_email')): ?>
-    <p><?php echo nl2br(sprintf($this->translate('ms_mail_product_purchased_no_email'), $this->sender, $this->order_info['firstname'] . ' ' . $this->order_info['lastname'], $order_info['eamil'], $order_info['shipping_method'], $products, $this->total)) ?></p>
+    <p><?php echo nl2br(sprintf($this->translate('ms_mail_product_purchased_no_email'), $this->sender, $this->order_info['firstname'] . ' ' . $this->order_info['lastname'], $order_info['email'], $order_info['shipping_method'], $products, $this->total)) ?></p>
 <?php else: ?>
     <p><?php echo nl2br(sprintf($this->translate('ms_mail_product_purchased'), $this->sender, $this->order_info['firstname'] . ' ' . $this->order_info['lastname'], $this->order_info['email'], $order_info['shipping_method'], $products, $this->total)) ?></p>
 <?php endif; ?>
